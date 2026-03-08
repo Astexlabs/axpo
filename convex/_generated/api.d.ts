243bcb47508db5e3ca36b3c60ef17ce6ab86143f
ableez <djayableez@gmail.com>
2026-03-08 16:08:25 +0000

    v1-1 iteration

---
+++ convex/_generated/api.d.ts
@@ -0,0 +1,20 @@
+/* eslint-disable */
+/**
+ * Generated `api` utility.
+ *
+ * THIS CODE IS AUTOMATICALLY GENERATED.
+ *
+ * To regenerate, run `npx convex dev`.
+ * @module
+ */
+
+import type { ApiFromModules, FilterApi, FunctionReference } from 'convex/server';
+import type * as emails from '../emails.js';
+import type * as users from '../users.js';
+
+declare const fullApi: ApiFromModules<{
+  emails: typeof emails;
+  users: typeof users;
+}>;
+export declare const api: FilterApi<typeof fullApi, FunctionReference<any, 'public'>>;
+export declare const internal: FilterApi<typeof fullApi, FunctionReference<any, 'internal'>>;
